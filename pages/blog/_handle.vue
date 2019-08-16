87118a49969af4b60a7889d6476699d752996f43
Andrew Laskey <andrew.laskey@gmail.com>
2019-08-16 09:46:36 -0700

    replace blog article components

--- pages/blog/_handle.vue
+++ pages/blog/_handle.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="article-page">
-    <article-view :article="article" :products="collection ? collection.products : []" />
+    <blog-article-view :article="article" :products="collection ? collection.products : []" />
   </div>
 </template>
 
 <script>
 import { getBlogArticle } from '@nacelle/nacelle-graphql-queries-mixins'
-import ArticleView from '~/components/ArticleView'
 
 export default {
-  components: {
-    ArticleView
-  },
   mixins: [getBlogArticle]
 }
 </script>
