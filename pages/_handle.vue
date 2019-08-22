8a4bf3c33e7b70e30f1d98252de486fef39a4e39
Andrew Laskey <andrew.laskey@gmail.com>
2019-08-22 16:32:48 -0700

    make homepage and page editable

--- pages/_handle.vue
+++ pages/_handle.vue
@@ -1,10 +1,79 @@
 <template>
-  <div>{{this.$route.params.handle}}</div>
+  <div class="page">
+    <div v-if="$apollo.loading">Loading...</div>
+    <page-content :page="page" :products="products">
+      <!-- 
+        /****
+        /* Customize Your Nacelle Content
+        /****
+      -->
+
+        <!-- <template v-slot:section="{ section }"> -->
+          
+          <!-- 
+            * Edit Hero Banner *
+                Available slots:
+                name: "background", data: "backgroundImgUrl", "mobileBackgroundImgUrl", "backgroundAltTag"
+                name: "body", data: "title", "subtitle", "textColor"
+                name: "cta", data: "ctaUrl", "ctaText", "ctaHandler"
+
+          <content-hero-banner
+            v-if="section.props.contentType === 'ContentHeroBanner'"
+            v-bind="section.props"
+          >
+            <template v-slot:body="{ title }">
+              <h1 class="special-title">{{ title }}</h4>
+            </template>
+          </content-hero-banner>
+          -->
+
+          <!--
+            * Edit Side-by-Side Section *
+                Available slots:
+                name: "body", data: "title", "copy"
+                name: "cta", data: "ctaUrl", "ctaText", "ctaHandler"
+
+          <content-side-by-side
+            v-if="section.props.contentType === 'ContentSideBySide'"
+            v-bind="section.props"
+          />
+          -->
+
+          <!--
+            * Edit Product Grid *
+                Available slots:
+                name: "header", data: "title"
+                name: "products", data: "products", "columns"
+
+          <content-product-grid
+            v-if="section.props.contentType === 'ContentProductGrid'"
+            v-bind="section.props"
+          />
+          -->
+
+          <!-- 
+            * Edit Testimonials *
+
+          <content-testimonials
+            v-if="section.props.contentType === 'ContentTestimonials'"
+            v-bind="section.props"
+          />
+          -->
+
+        <!-- </template> -->
+    </page-content>
+  </div>
 </template>
 
 <script>
-export default {}
-</script>
+import { getPage } from '@nacelle/nacelle-graphql-queries-mixins'
 
-<style>
-</style>
+export default {
+  mixins: [getPage],
+  computed: {
+    handle() {
+      return this.$route.params.handle
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,78 @@
 <template>
   <div class="page">
     <div v-if="$apollo.loading">Loading...</div>
-    <component :is="sourceComponent" :content="pageContent" :products="pageProducts" />
+    <page-content :page="page" :products="products">
+      <!-- 
+        /****
+        /* Customize Your Nacelle Content
+        /****
+      -->
+
+        <!-- <template v-slot:section="{ section }"> -->
+          
+          <!-- 
+            * Edit Hero Banner *
+                Available slots:
+                name: "background", data: "backgroundImgUrl", "mobileBackgroundImgUrl", "backgroundAltTag"
+                name: "body", data: "title", "subtitle", "textColor"
+                name: "cta", data: "ctaUrl", "ctaText", "ctaHandler"
+
+          <content-hero-banner
+            v-if="section.props.contentType === 'ContentHeroBanner'"
+            v-bind="section.props"
+          >
+            <template v-slot:body="{ title }">
+              <h1 class="special-title">{{ title }}</h4>
+            </template>
+          </content-hero-banner>
+          -->
+
+          <!--
+            * Edit Side-by-Side Section *
+                Available slots:
+                name: "body", data: "title", "copy"
+                name: "cta", data: "ctaUrl", "ctaText", "ctaHandler"
+
+          <content-side-by-side
+            v-if="section.props.contentType === 'ContentSideBySide'"
+            v-bind="section.props"
+          />
+          -->
+
+          <!--
+            * Edit Product Grid *
+                Available slots:
+                name: "header", data: "title"
+                name: "products", data: "products", "columns"
+
+          <content-product-grid
+            v-if="section.props.contentType === 'ContentProductGrid'"
+            v-bind="section.props"
+          />
+          -->
+
+          <!-- 
+            * Edit Testimonials *
+
+          <content-testimonials
+            v-if="section.props.contentType === 'ContentTestimonials'"
+            v-bind="section.props"
+          />
+          -->
+
+        <!-- </template> -->
+    </page-content>
   </div>
 </template>
 
 <script>
 import { getPage } from '@nacelle/nacelle-graphql-queries-mixins'
+
 export default {
   mixins: [getPage],
   computed: {
-    sourceComponent() {
-      if (this.page && this.page.source) {
-        if (this.page.source === 'shopify') {
-          return 'ShopifyPageContent'
-        }
-      }
-
-      return 'div'
-    },
-    pageProducts() {
-      if (this.page && this.page.products && this.page.products.length > 0) {
-        return this.page.products
-      }
-
-      return null
-    },
-    pageContent() {
-      if (this.page && this.page.content && this.page.content.length > 0) {
-        return this.page.content
-      }
-
-      return null
+    handle() {
+      return 'homepage'
     }
   }
 }
