1586ac886e5c04b9865f6181d14d46903a498ea9
Andrew Laskey <andrew.laskey@gmail.com>
2019-10-08 10:56:42 -0700

    update nacelle module and CartItem

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="columns is-marginless is-mobile flyout-cart-item">
-    <router-link class="column is-3" :to="`${pathFragment}${item.handle}`" @click.native="hideCart">
-      <product-image :source="item.image.thumbnailSrc" :alt="item.title" />
+    <router-link
+      :to="`${pathFragment}${item.handle}`"
+      class="column is-3"
+      @click.native="hideCart"
+    >
+      <product-image
+        v-if="productThumbnail && productThumbnail.length > 0"
+        :source="productThumbnail"
+        :alt="item.title"
+      />
     </router-link>
 
     <div class="column is-9">
       <router-link :to="`${pathFragment}${item.handle}`" @click.native="hideCart">
         <product-title class="flyout-item-title" element="h4" :title="item.title" />
       </router-link>
-      <product-variant-title class="flyout-item-variant-title" :title="item.variant.title" />
+      <product-variant-title
+        :title="variant.title"
+        class="flyout-item-variant-title"
+      />
       <div class="flyout-item-details columns is-marginless is-paddingless">
-        <product-price class="flyout-item-price" :price="item.variant.price" />
+        <product-price class="flyout-item-price" :price="variant.price" />
         <product-quantity-update :product="item" :variant="item.variant" />
-        <cart-flyout-item-remove-button :variantId="item.variant.id" />
+        <cart-flyout-item-remove-button :variantId="variant.id" />
       </div>
     </div>
   </div>
@@ -29,6 +40,38 @@ export default {
     pathFragment: {
       type: String,
       default: '/products/'
+    }
+  },
+  computed: {
+    productThumbnail () {
+      if (
+        this.item &&
+        this.item.image &&
+        this.item.image.thumbnailSrc
+      ) {
+        return this.item.image.thumbnailSrc
+      }
+
+      return ''
+    },
+    variant () {
+      const defaultVariant = {
+        id: '',
+        title: '',
+        price: 0
+      }
+
+      if (
+        this.item &&
+        this.item.variant
+      ) {
+        return {
+          ...defaultVariant,
+          ...this.item.variant
+        }
+      }
+
+      return defaultVariant
     }
   },
   methods: {
