b436181b6dc14fd5d80fab2e23c10c4cf4bd4945
Nick Richmond <nwrichmond@turnt.tech>
2019-12-24 10:33:34 -0500

    Add placeholder privacy policy

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="page">
+    <div class="container">
+      <p>
+        It's important to provide your users with an explanation 
+        of your cookie policy in order to comply with data 
+        privacy laws such as
+        <a
+          href="https://gdpr.eu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >GDPR</a> and
+        <a href="https://oag.ca.gov/privacy/ccpa" target="_blank" rel="noopener noreferrer">CCPA</a>.
+        As a merchant, it's your responsibility to have an
+        active command of the cookies served by your site, and to
+        communicate to your users:
+        <ul>
+          <li>the purpose and scope of cookies on your site</li>
+          <li>how and why information about your users is collected</li>
+          <li>what kind of information about your users is collected</li>
+          <li>when information about your users is collected</li>
+          <li>how information about your users is used</li>
+          <li>for how long information about your users is stored</li>
+          <li>how to opt out of personal data collection, in part or in full</li>
+        </ul>
+      </p>
+      <p>
+        Your obligations to disclose information about personal
+        data collection will vary depending on where you do business. 
+        Please note that the data privacy communication obligations 
+        listed above are only provided as suggestions of industry best
+        practices. While these reccomendations are a good starting 
+        point, it is the merchant's responsibility to seek out and 
+        act on the legal advice of an attorney before collecting or
+        processing users' personal information.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 800px;
+  padding-top: 2em;
+  line-height: 1.5;
+  margin-bottom: 1em;
+  p {
+    margin-bottom: 0.5em;
+  }
+  ul {
+    list-style-type: circle;
+    margin-top: 0.5em;
+    margin-left: 2em;
+  }
+}
+</style>
