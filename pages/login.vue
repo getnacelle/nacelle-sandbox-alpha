a28362535b3061f73d21f12380d0273ac52ec545
Andrew Laskey <andrew.laskey@gmail.com>
2020-02-04 14:09:54 -0800

    testing login form

---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <div class='oxi-social-login'></div>
+    <script src="https://social-login.oxiapps.com/api/init?vt=928348&shop=starship-furniture.myshopify.com" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .login {
+    margin: 0 auto;
+    padding: 3rem 1rem;
+    max-width: 400px;
+  }
+</style>
