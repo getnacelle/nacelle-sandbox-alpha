94e4baa043d3d65892868253471851d3cffa1e67
Andrew Laskey <andrew.laskey@gmail.com>
2020-02-04 19:05:59 -0800

    moved oxi login iframe into component

---
+++ components/OxiSocialLogin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login">
+    <div 
+      v-if="showIframe"
+      class="oxi_social_wrapper"
+    >
+      <iframe
+        id="social_login_frame"
+        :src="iframeSrc"
+        style="width:100%;max-width:100%;padding-top:0px;margin-bottom:5px;border:0px;height:240px;"
+        scrolling=no
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // oxiAccountId: {
+    //   type: String,
+    //   default: ''
+    // },
+    shopifyDomain: {
+      type: String,
+      default: ''
+    },
+    shopifyAccountPage: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    showIframe() {
+      return (
+        this.shopifyDomain.length > 0 &&
+        this.shopifyAccountPage.length > 0
+      )
+    },
+    iframeSrc() {
+      const endpoint = 'https://social-login.oxiapps.com/widget'
+      const params = `site=${this.shopifyDomain}&parenturl=${this.shopifyAccountPage}`
+      return `${endpoint}?${params}`
+    }
+  }
+}
+</script>
+
+<style>
+  .login {
+    margin: 0 auto;
+    padding: 3rem 1rem;
+    max-width: 400px;
+  }
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="login">
-    <div class='oxi-social-login'></div>
-    <script src="https://social-login.oxiapps.com/api/init?vt=928348&shop=starship-furniture.myshopify.com" />
+  <div class="login-page">
+    <oxi-social-login
+      oxi-account-id="928348"
+      shopify-domain="starship-furniture.myshopify.com"
+      shopify-account-page="https://shopdemo.getnacelle.com/account/"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import OxiSocialLogin from '~/components/OxiSocialLogin.vue'
 
+export default {
+  components: {
+    OxiSocialLogin
+  }
 }
 </script>
-
-<style>
-  .login {
-    margin: 0 auto;
-    padding: 3rem 1rem;
-    max-width: 400px;
-  }
-</style>
